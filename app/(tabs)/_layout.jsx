e95818224292a2f00bfc0f63dc75366dc52f8c97
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2023-11-24 20:11:32 +0700

    update hotel

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -29,9 +29,9 @@ export default function AppLayout() {
         }}
       />
       <Tabs.Screen
-        name="booking"
+        name="history"
         options={{
-          title: 'Booking',
+          title: 'History',
           tabBarIcon: ({ focused }) =>
             focused ? (
               <Ionicons name="notifications" size={24} color="#003580" />
@@ -66,46 +66,6 @@ export default function AppLayout() {
           href: null,
         }}
       />
-      {/* <Tabs.Screen
-        name="favorites"
-        options={{
-          title: "Favorites",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="heart" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="cog" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="login"
-        options={{
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="details"
-        options={{
-          headerShown: false,
-          href: null,
-        }}
-      />
       <Tabs.Screen
         name="booking"
         options={{
@@ -113,13 +73,6 @@ export default function AppLayout() {
           href: null,
         }}
       />
-      <Tabs.Screen
-        name="search"
-        options={{
-          headerShown: false,
-          href: null,
-        }}
-      /> */}
     </Tabs>
   );
 }
